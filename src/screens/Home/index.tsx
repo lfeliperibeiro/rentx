54bf3077715d8c834cbbcff64fb34cd6e7a4fd87
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-30 20:56:32 -0300

    feat: add safe area view in IOS

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { Platform, SafeAreaView, StatusBar } from "react-native";
 
 import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 import Logo from "../../assets/logo.svg";
 import { RFValue } from "react-native-responsive-fontsize";
+import theme from "../../styles/theme";
 
 export function Home() {
   return (
     <Container>
+      {Platform.OS === "ios" ? (
+        <SafeAreaView style={{ backgroundColor: theme.colors.header }} />
+      ) : null}
       <StatusBar
         barStyle={"light-content"}
         backgroundColor={"transparent"}
