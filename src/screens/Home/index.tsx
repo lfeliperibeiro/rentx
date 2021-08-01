a4acccfd56ccf13a501b009ee113aa4a6108a40b
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-01 00:30:44 -0300

    feat: pass Car component for Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,8 +6,31 @@ import { Container, Header, TotalCars, HeaderContent } from "./styles";
 import Logo from "../../assets/logo.svg";
 import { RFValue } from "react-native-responsive-fontsize";
 import theme from "../../styles/theme";
+import { Car } from "../../components/Car";
 
 export function Home() {
+  const carDataOne = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "Ao dia",
+      price: 120,
+    },
+    thumbnail:
+      "https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png?fit=around%7C875:492.1875",
+  };
+
+  const carDataTwo = {
+    brand: "Porshe",
+    name: "Huracan",
+    rent: {
+      period: "Ao dia",
+      price: 340,
+    },
+    thumbnail:
+      "https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png?fit=around%7C875:492.1875",
+  };
+
   return (
     <Container>
       {Platform.OS === "ios" ? (
@@ -24,6 +47,8 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
