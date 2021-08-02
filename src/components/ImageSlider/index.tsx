390d9f0e858084afe2b7d6ac147e84379e4ce423
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-02 13:52:36 -0300

    feat: create image slider component

---
+++ src/components/ImageSlider/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Container,
+  ImagesIndexes,
+  ImageIndex,
+  CarImageWrapper,
+  CarImage,
+} from "./styles";
+
+interface Props {
+  imageUrl: string[];
+}
+export function ImageSlider({ imageUrl }: Props) {
+  return (
+    <Container>
+      <ImagesIndexes>
+        <ImageIndex active={true} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+        <ImageIndex active={true} />
+      </ImagesIndexes>
+      <CarImageWrapper>
+        <CarImage source={{ uri: imageUrl[0] }} resizeMode={"contain"} />
+      </CarImageWrapper>
+    </Container>
+  );
+}
