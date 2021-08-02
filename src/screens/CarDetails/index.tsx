4125bdb2abe3d82d68b06cf753ee70bb39067ebd
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-02 20:48:50 -0300

    feat: create Button componente

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { Container, Header, CarImages } from "./styles";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from "./styles";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
 import { BackButton } from "../../components/BackButton";
 import { StatusBar } from "react-native";
 
 import { ImageSlider } from "../../components/ImageSlider";
+import { Accessory } from "../../components/Accessory";
+import { Button } from "../../components/Button";
 
 export function CarDetails() {
   return (
@@ -23,6 +48,35 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          <Accessory name={"300/km/h"} icon={SpeedSvg} />
+          <Accessory name={"3.2s"} icon={AccelerationSvg} />
+          <Accessory name={"800 HP"} icon={ForceSvg} />
+          <Accessory name={"Gasolina"} icon={GasolineSvg} />
+          <Accessory name={"Auto"} icon={ExchangeSvg} />
+          <Accessory name={"2 pessoas"} icon={PeopleSvg} />
+        </Accessories>
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title={"Confirmar"} />
+      </Footer>
     </Container>
   );
 }
