60b46421897a4a4d11605a158c9a0fe11e273f5d
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-06 20:59:04 -0300

    feat: add Mycars route

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import { CarDetails } from "../screens/CarDetails";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
 import { Schedule } from "../screens/Schedule";
+import { MyCars } from "../screens/MyCars";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -16,6 +17,7 @@ export function StackRoutes() {
       <Screen name={"Schedule"} component={Schedule} />
       <Screen name={"SchedulingDetails"} component={SchedulingDetails} />
       <Screen name={"SchedulingComplete"} component={SchedulingComplete} />
+      <Screen name={"MyCars"} component={MyCars} />
     </Navigator>
   );
 }
