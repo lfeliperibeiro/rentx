828b28734726d8d99e4745935e51dc2b79b26448
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-06 20:57:45 -0300

    feat: create MyCarButton

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,23 +2,35 @@ import React, { useEffect, useState } from "react";
 import { Platform, SafeAreaView, StatusBar } from "react-native";
 import api from "../../services/api";
 import { CarDTO } from "../../dtos/carDTO";
-
-import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent,
+  CarList,
+  MyCarsButton,
+} from "./styles";
 
 import Logo from "../../assets/logo.svg";
 import { RFValue } from "react-native-responsive-fontsize";
-import theme from "../../styles/theme";
 import { Car } from "../../components/Car";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { Load } from "../../components/Load";
+import { useTheme } from "styled-components";
 
 export function Home() {
+  const theme = useTheme();
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
   const navigation: NavigationProp<any> = useNavigation();
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate("CarDetails", { car });
+  }
+
+  function handleMyCars() {
+    navigation.navigate("MyCars");
   }
 
   useEffect(() => {
@@ -62,6 +74,9 @@ export function Home() {
           )}
         />
       )}
+      <MyCarsButton onPress={handleMyCars}>
+        <Ionicons name={"ios-car-sport"} size={32} color={theme.colors.shape} />
+      </MyCarsButton>
     </Container>
   );
 }
